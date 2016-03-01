7eab5c11aa24f38064870b0a0ba88526e2a9be60
jernejsk <jernej.skrabec@siol.net>
2016-03-01 19:27:05 +0100

    Add libvdpau-sunxi package without X11 dependency

--- packages/multimedia/libvdpau-sunxi/package.mk
+++ packages/multimedia/libvdpau-sunxi/package.mk
@@ -16,24 +16,23 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="libcedrus"
-PKG_VERSION="e129e79337fc9073ddaf56fc680f2338dac8960b"
+PKG_NAME="libvdpau-sunxi"
+PKG_VERSION="685769372a8281bf67046a958883323aaf14d232"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/linux-sunxi/libcedrus"
-PKG_URL="https://github.com/linux-sunxi/libcedrus/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_SITE="https://github.com/linux-sunxi/libvdpau-sunxi"
+PKG_URL="https://github.com/linux-sunxi/libvdpau-sunxi/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libcedrus libvdpau"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
-PKG_SHORTDESC="libcedrus: sunxi Video Engine library"
-PKG_LONGDESC="libcedrus: sunxi Video Engine library"
+PKG_SHORTDESC="libvdpau-sunxi: Experimental VDPAU for Allwinner sunxi SoCs (WiP)"
+PKG_LONGDESC="libvdpau-sunxi: Experimental VDPAU for Allwinner sunxi SoCs (WiP)"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_MAKEINSTALL_OPTS_TARGET="prefix=/usr"
-
 unpack() {
   tar xf $ROOT/$SOURCES/$PKG_NAME/$PKG_VERSION.tar.gz -C $ROOT/$BUILD
 }
+	
--- projects/H3/packages/libvdpau/package.mk
+++ projects/H3/packages/libvdpau/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,24 +16,22 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="libcedrus"
-PKG_VERSION="e129e79337fc9073ddaf56fc680f2338dac8960b"
+PKG_NAME="libvdpau"
+PKG_VERSION="1.1.1"
 PKG_REV="1"
 PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/linux-sunxi/libcedrus"
-PKG_URL="https://github.com/linux-sunxi/libcedrus/archive/$PKG_VERSION.tar.gz"
+PKG_LICENSE="MIT"
+PKG_SITE="https://wiki.freedesktop.org/www/Software/VDPAU/"
+PKG_URL="https://secure.freedesktop.org/~aplattner/vdpau/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
-PKG_SHORTDESC="libcedrus: sunxi Video Engine library"
-PKG_LONGDESC="libcedrus: sunxi Video Engine library"
+PKG_SHORTDESC="libvdpau: a Video Decode and Presentation API for UNIX."
+PKG_LONGDESC="VDPAU is the Video Decode and Presentation API for UNIX. It provides an interface to video decode acceleration and presentation hardware present in modern GPUs."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-PKG_MAKEINSTALL_OPTS_TARGET="prefix=/usr"
-
-unpack() {
-  tar xf $ROOT/$SOURCES/$PKG_NAME/$PKG_VERSION.tar.gz -C $ROOT/$BUILD
-}
+PKG_CONFIGURE_OPTS_TARGET="--disable-dri2 \
+                           --disable-documentation \
+                           --with-module-dir=/usr/lib/vdpau"
